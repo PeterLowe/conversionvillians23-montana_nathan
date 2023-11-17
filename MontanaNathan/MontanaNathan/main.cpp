f150f67d5cdfbe971095e9a3254b85603e280b5e
(Student  C00296838) Nathan Mas <C00296838@setu.ie>
2023-11-17 10:33:59 +0000

    Added code

--- MontanaNathan/MontanaNathan/main.cpp
+++ MontanaNathan/MontanaNathan/main.cpp
@@ -2,8 +2,49 @@
 //Nathan Mas
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+char printmenu(char t_newprintmenu);
+#include <iostream>
 int main()
 {
-
 	return 1;
+}
+
+char printmenu()
+{
+	char choice = ' ';
+	std::cout << "Please enter an upper case letter to convert from left to right/nand lower to convert from right to left/n/n";
+	std::cout << "[D] Mpg <---> L/100km [d]" << std::endl;
+	std::cout << "[E] Kilograms <---> Stone & Pounds [e]" << std::endl;
+	std::cout << "[F] Celsius <---> Fahrenheit [f]" << std::endl;
+	std::cout << "[Q] Exit [---] Exit [q]" << std::endl;
+	std::cout << "Enter your choice: ";
+	std::cin >> choice;
+	return choice;
 }
