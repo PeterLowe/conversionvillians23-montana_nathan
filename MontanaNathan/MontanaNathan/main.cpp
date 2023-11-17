dfe215fa7c2ab8d0c69a5c84e3c66fbf0620d235
(Student  C00297005) Montana Nicholas <C00297005@setu.ie>
2023-11-17 10:18:19 +0000

    Added text

--- MontanaNathan/MontanaNathan/main.cpp
+++ MontanaNathan/MontanaNathan/main.cpp
@@ -1,9 +1,20 @@
 //Montana Nicholas
 
-
+#include <iostream>
 
 int main()
-{
+char printMenu() //written by leader
 
-	return 1;
+{
+	char choice = '';
+	std::cout << "Please enter an upper case letter to convert from left to right\nand lower to convert from right to left\n\n" << std::endl;
+	std::cout << "[A] Degrees <---> Radians [a]" << std:endl;
+	std::cout << "[B] Centimeters <---> Feet & Inches [b]" << std::endl;
+	std::cout << "[C] Litres <---> Gallons [c]" << std::endl;
+
+	std::cout << "[Q] Exit [---] Exit [q]" << std::endl;
+	std::cout << "Enter your choice: ";
+	std::cin >> choice;
+
+	return choice;
 }
